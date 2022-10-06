f284dbc4c7facd620e8b2256589fcf57d21efb4e
Ioannis Daniil <jondan97@gmail.com>
2022-10-06 10:39:00 +0300

    added h2 database & attempted connection

--- src/main/java/edu/acme/Main.java
+++ src/main/java/edu/acme/Main.java
@@ -7,13 +7,16 @@ import edu.acme.model.University;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         final UniversityFactory universityFactory = new UniversityFactory();
         final List<University> universities = universityFactory.getSampleData();
         universities.forEach(university -> log.info("University: {}", university));
@@ -21,5 +24,10 @@ public class Main {
         final StudentFactory studentFactory = new StudentFactory(universities);
         final List<Student> students = studentFactory.getSampleData();
         students.forEach(student -> log.info("Student: {}", student));
+
+
+        Class.forName ("org.h2.Driver");
+        Connection conn = DriverManager.getConnection ("jdbc:h2:mem:university", "sa","");
+        conn.close();
     }
 }

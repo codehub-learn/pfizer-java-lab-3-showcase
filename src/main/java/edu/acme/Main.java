4419b974c03954004cbd3392b768ebee979ab449
Ioannis Daniil <jondan97@gmail.com>
2022-10-13 10:37:14 +0300

    added logic for table creation

--- src/main/java/edu/acme/Main.java
+++ src/main/java/edu/acme/Main.java
@@ -32,10 +32,63 @@ public class Main {
         try (Connection connection = DatabaseSourceConnectionPooling.getConnection()){
             String productVersion = connection.getMetaData().getDatabaseProductVersion();
             log.info(productVersion);
+        }
 
-            Statement statement = connection.createStatement();
-            int i = statement.executeUpdate("CREATE TABLE UNIVERSITY");
+        List<String> sqlCreateQueries = getTableCreationQueries();
+
+        sqlCreateQueries.forEach(query -> {
+            try {
+                createTable(query);
+            } catch (SQLException e) {
+                log.error("Something went wrong during the creation of the tables");
+            }
+        });
+
+    }
+
+    private static void createTable(String sql) throws SQLException {
+        try (Connection connection = DatabaseSourceConnectionPooling.getConnection();
+             Statement statement = connection.createStatement()){
+            int i = statement.executeUpdate(sql);
             log.info("{}", i);
         }
+    }
+
+    private static List<String> getTableCreationQueries(){
+        String universitySql = "CREATE TABLE UNIVERSITY " +
+                "(id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR2(50) NOT NULL)";
+
+        String departmentSql = "CREATE TABLE DEPARTMENT " +
+                "(id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                "name VARCHAR2(50) NOT NULL, " +
+                "university_id INTEGER NOT NULL, " +
+                "foreign key (university_id) references UNIVERSITY(id))";
+
+        String unitSql = "CREATE TABLE UNIT (" +
+                "  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                "  name VARCHAR2(100), " +
+                "  tutor_name VARCHAR2(100), " +
+                "  department_id INTEGER NOT NULL, " +
+                "  foreign key (department_id) references DEPARTMENT(id)" +
+                "  )";
+
+        String studentSql = "CREATE TABLE STUDENT (" +
+                "  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                "  name VARCHAR2(100) NOT NULL, " +
+                "  address VARCHAR2(100) NOT NULL, " +
+                "  dateOfBirth DATE NOT NULL " +
+                "  )";
+
+        String enrollmentSql = "CREATE TABLE ENROLLMENT (" +
+                "  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, " +
+                "  grade NUMBER(3), " +
+                "  student_id INTEGER NOT NULL," +
+                "  unit_id INTEGER NOT NULL," +
+                "  foreign key (student_id) references STUDENT(id), " +
+                "  foreign key (unit_id) references UNIT(id) " +
+                "  )";
+
+        return List.of(universitySql, departmentSql, unitSql, studentSql, enrollmentSql);
     }
 }
